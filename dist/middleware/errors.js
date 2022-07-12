3c67871fab89bb7657e57d9047fb9979332f4a4a
shermondevelopment <victorshermon@yahoo.com>
2022-07-11 22:12:14 -0300

    removendo pagina dist do repositorio

--- dist/middleware/errors.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (error, req, res, next) => {
-    res.status(error.status).json({ error: error.message });
-};
